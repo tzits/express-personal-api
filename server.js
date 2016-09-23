18c409662f3322fcdf53a27e3c0eb039200567d0
Toby <tzitsman@gmail.com>
2016-09-23 13:54:04 -0700

    endpoints
    changed some endpoints...i hope

--- server.js
+++ server.js
@@ -45,14 +45,15 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
+    woopsIForgotToDocumentAllMyEndpoints: false,
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/tzits/express-personal-api",
+    baseUrl: "https://desolate-castle-72927.herokuapp.com/",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "The start of my memoir"},
+      {method: "POST", path: "/api/bookseries", description: "Let start rating some series"}
+      {method: "GET", path: "/api/comedies", description: "Trying to help educate my generation"}
     ]
   })
 });
